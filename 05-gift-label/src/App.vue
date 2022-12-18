0302dfad4ddc4c80a4853549a326d75609d7cb47
niweb <n.weber@marathon-berlin.de>
2022-12-18 22:59:58 +0100

    Implement day 05

--- 05-gift-label/src/App.vue
+++ 05-gift-label/src/App.vue
@@ -6,15 +6,25 @@
           <img src="/assets/santa.svg" alt="" class="max-w-[12em]" />
           <div>
             <h1 class="font-serif text-4xl">Happy Holidays!</h1>
-            <div class="mt-4 bg-white text-black p-2 rounded">TO: John</div>
-            <div class="mt-2 bg-white text-black p-2 rounded">FROM: Joana</div>
+            <div class="mt-4 bg-white text-black p-2 rounded">TO: {{ to }}</div>
+            <div class="mt-2 bg-white text-black p-2 rounded">FROM: {{ from }}</div>
           </div>
         </div>
       </div>
+      <label class="text-lg">
+        To:
+        <input v-model="to" type="text" class="shadow border rounded w-full py-2 px-3" />
+      </label>
+      <label class="text-lg">
+        From:
+        <input v-model="from" type="text" class="shadow border rounded w-full py-2 px-3" />
+      </label>
     </div>
   </div>
 </template>
 
 <script setup>
-// ...
+import { ref } from 'vue'
+const to = ref('')
+const from = ref('')
 </script>
