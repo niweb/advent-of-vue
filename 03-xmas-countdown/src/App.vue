643d3fb21bf38dc9dd1c43fef3fe2d7852de06c7
niweb <n.weber@marathon-berlin.de>
2022-12-18 22:39:08 +0100

    Implement day 03

--- 03-xmas-countdown/src/App.vue
+++ 03-xmas-countdown/src/App.vue
@@ -2,9 +2,27 @@
 import CountdownHeader from '@/components/CountdownHeader.vue'
 import CountdownSegment from './components/CountdownSegment.vue'
 import { useNow } from '@vueuse/core'
+import { computed } from 'vue';
 
 const now = useNow()
 const christmas = new Date('12/25/2022 00:00:00')
+const diff = computed(() => christmas - now.value)
+
+const secondInMs = 1000;
+const minuteInMs = secondInMs * 60;
+const hourInMs = minuteInMs * 60;
+const dayInMs = hourInMs * 24
+
+const days = computed(() => Math.floor(diff.value / dayInMs))
+const diffWithoutDays = computed(() => diff.value - days.value * dayInMs)
+
+const hours = computed(() => Math.floor(diffWithoutDays.value / hourInMs))
+const diffWithoutHours = computed(() => diffWithoutDays.value - hours.value * hourInMs)
+
+const minutes = computed(() => Math.floor(diffWithoutHours.value / minuteInMs))
+const diffWithoutMinutes = computed(() => diffWithoutHours.value - minutes.value * minuteInMs)
+
+const seconds = computed(() => Math.floor(diffWithoutMinutes.value / secondInMs))
 </script>
 <template>
   <div class="w-full h-full flex justify-center items-center p-10">
@@ -12,10 +30,10 @@ const christmas = new Date('12/25/2022 00:00:00')
       <div class="shadow-md relative bg-white p-5 rounded-lg border-gray-100 border-[1px]">
         <CountdownHeader />
         <main class="flex justify-center">
-          <CountdownSegment label="days" number="00" />
-          <CountdownSegment label="hours" number="00" />
-          <CountdownSegment label="minutes" number="00" />
-          <CountdownSegment label="seconds" number="00" />
+          <CountdownSegment label="days" :number="days" />
+          <CountdownSegment label="hours" :number="hours" />
+          <CountdownSegment label="minutes" :number="minutes" />
+          <CountdownSegment label="seconds" :number="seconds" />
         </main>
       </div>
       <h4 class="mt-10 text-gray-400 text-center text-sm">
--- 03-xmas-countdown/src/components/CountdownSegment.vue
+++ 03-xmas-countdown/src/components/CountdownSegment.vue
@@ -7,7 +7,9 @@ defineProps({
 <template>
   <div class="segment text-center">
     <div class="pt-10 overflow-hidden relative">
-      <span :key="number" class="numbers text-green absolute top-0 left-[50%]">{{ number }}</span>
+      <Transition>
+        <span :key="number" class="numbers text-green absolute top-0 left-[50%]">{{ number }}</span>
+      </Transition>
     </div>
 
     <span class="label block pt-2">{{ label }}</span>
@@ -23,5 +25,17 @@ defineProps({
 }
 .label {
   font-size: 16px;
+}
+
+.v-enter-active, .v-leave-active {
+  transition: transform 0.5s ease-out;
+}
+
+.v-enter-from {
+  transform: translateY(-75%) translateX(-50%);
+}
+
+.v-leave-to {
+  transform: translateY(75%) translateX(-50%);
 }
 </style>
