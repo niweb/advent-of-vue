39c3b76b39c2ec681c6038115b131eb8d3ddd10b
niweb <n.weber@marathon-berlin.de>
2022-12-18 22:47:31 +0100

    Implement day 04

--- 04-recursive-christmas-tree/src/ChristmasTree.vue
+++ 04-recursive-christmas-tree/src/ChristmasTree.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
+    <ChristmasTree v-if="size > 1" :size="size - 1"></ChristmasTree>
     <div class="flex flex-row justify-center">
       <!-- This div is styled for the tree sections, but it's not complete -->
-      <div class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
+      <div v-for="_ in size" class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
     </div>
   </div>
 </template>
