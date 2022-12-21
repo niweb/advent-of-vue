54ec8c2a8a0f5aa3c0e76c1fd0142dc351472869
niweb <n.weber@marathon-berlin.de>
2022-12-21 14:27:52 +0100

    Implement day 09

--- 09-sort-presents/src/App.vue
+++ 09-sort-presents/src/App.vue
@@ -1,22 +1,25 @@
 <template>
-  <div class="w-full h-full p-4 flex justify-center items-center">
+  <div class="w-full h-full p-4 flex flex-col space-y-12 justify-center items-center">
     <div class="max-w-md">
       <div>
         <img src="/assets/tree.svg" alt="Christmas tree" />
       </div>
       <div class="mt-2 flex justify-center items-center">
-        <img
-          v-for="present in presents"
-          :key="present.id"
-          :src="present.src"
-          :alt="`Present ${present.id}`"
-          data-qa="present"
-        />
+        <img v-for="present in (sort ? sortedPresents : presents)" :key="present.id" :src="present.src"
+          :alt="`Present ${present.id}`" data-qa="present" />
       </div>
     </div>
+    <button @click="sort = !sort" class="bg-green text-white py-2 px-4 rounded">Toggle sort</button>
   </div>
 </template>
 
 <script setup>
+import { computed, ref } from 'vue'
 import presents from './presents.json'
+
+const sort = ref(false)
+const sortedPresents = computed(() => [...presents].sort(
+  (a, b) => (a.dimensions.width * a.dimensions.height) - (b.dimensions.width * b.dimensions.height)
+))
+
 </script>
