fc1d57ad13667b08438714fea13c81f98f1aa111
niweb <n.weber@marathon-berlin.de>
2022-12-21 13:58:21 +0100

    Implement day 08

--- 08-i18n-happy-holidays/src/App.vue
+++ 08-i18n-happy-holidays/src/App.vue
@@ -1,42 +1,62 @@
 <template>
   <main class="flex flex-col justify-center h-full mx-auto max-w-600px">
     <section class="flex flex-col items-center leading-loose text-center">
-      <div class="text-3xl">
+      <div class="text-3xl mb-4">
         <span class="i-twemoji-christmas-tree"></span>
-        Happy Holidays!
+        {{ $t('happyHolidays') }}
         <span class="i-twemoji-world-map"></span>
       </div>
-      <!-- Dates - Check out locales/en.json for the key -->
-      <!-- Controls - I give you an .icon-button class if you want to use it -->
-      <!-- Flags - the current locale -->
+      <div class="text-xl mb-4">
+        <i18n-t keypath="christmasIsComing">
+    <template #date>
+      <span>{{ $d(christmasDate) }}</span>
+    </template>
+    <template #time>
+      <span class="text-green">{{ $t('day', { count: daysUntilChristmas }) }}</span>
+    </template>
+  </i18n-t>
+      </div>
+      <div class="flex space-x-16">
+        <button class="icon-button" @click="onLocaleChange">
+          <span class="i-twemoji-globe-with-meridians" />
+        </button>
+        <div class="space-x-2">
+          <span :class="flags[locale]" />
+          <span>{{ $t('language') }}</span>
+        </div>
+      </div>
     </section>
   </main>
 </template>
 
 <script setup>
 import { useI18n } from 'vue-i18n'
+import { computed, ref } from 'vue'
+import { useNow } from '@vueuse/core'
 
-// See the README about tricky timezone issues!
-// I figured since this is i18n-friendly, we'd wanna
-// make sure the timezones were right :-)
+const { locale } = useI18n()
+
+const now = useNow()
 const christmasDate = new Date('2022/12/25')
+const daysUntilChristmas = computed(() => Math.ceil((christmasDate - now.value) / (1000 * 60 * 60 * 24)))
+
+const locales = ['en', 'de', 'ja-JP']
+const curentLocale = ref(0)
+const onLocaleChange = () => {
+  const currentIndex = locales.findIndex(i => i === locale.value)
+  const nextIndex = currentIndex + 1 % locales.length;
+  locale.value = locales[nextIndex]
+}
+
+const flags = {
+  'en': 'i-twemoji-flag-united-states',
+  'de': 'i-twemoji-flag-germany',
+  'ja-JP': 'i-twemoji-flag-japan',
+}
 </script>
 
 <style scoped>
 .icon-button {
-  @apply text-xl
-    w-32px
-    h-32px
-    rounded-full
-    border-1
-    border-transparent
-    bg-transparent
-    cursor-pointer
-    duration-300
-    hover:ring-2
-    hover:border-green-500
-    hover:ring-green-500
-    hover:ring-opacity-40
-    hover:text-green-600;
+  @apply text-xl w-32px h-32px rounded-full border-1 border-transparent bg-transparent cursor-pointer duration-300 hover: ring-2 hover:border-green-500 hover:ring-green-500 hover:ring-opacity-40 hover:text-green-600;
 }
 </style>
