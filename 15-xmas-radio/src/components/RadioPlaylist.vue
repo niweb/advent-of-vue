38ceb18733a1dacce4c18c6104f5396022c517e1
Nina Weber <niweb@MBP-von-Nina.fritz.box>
2022-12-27 16:48:22 +0100

    Add day 15

---
+++ 15-xmas-radio/src/components/RadioPlaylist.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { usePlaylist } from '@/composables/usePlaylist'
+import { useCurrentTrack } from '@/composables/useCurrentTrack'
+import RadioSong from '@/components/RadioSong.vue'
+
+const { songs, state } = usePlaylist()
+const { playing, play } = useCurrentTrack()
+
+function isSelected(song) {
+  return song.slug === state.value.slug
+}
+function isPlaying(song) {
+  return isSelected(song) && playing.value
+}
+</script>
+<template>
+  <ul>
+    <RadioSong
+      v-for="song in songs"
+      :key="song.slug"
+      :song="song"
+      :isSelected="isSelected(song)"
+      :isPlaying="isPlaying(song)"
+      @click="play(song)"
+    />
+  </ul>
+</template>
