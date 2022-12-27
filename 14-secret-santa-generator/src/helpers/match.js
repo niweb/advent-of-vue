ac990a873de3d5362ceda56d3283c8799b8af788
niweb <n.weber@marathon-berlin.de>
2022-12-27 18:14:03 +0100

    Implement day 14

--- 14-secret-santa-generator/src/helpers/match.js
+++ 14-secret-santa-generator/src/helpers/match.js
@@ -8,15 +8,45 @@ export const hasDuplicates = list => {
   return hasDuplicates(nameList) || hasDuplicates(emailList)
 }
 
+const getRandomInt = (max) => Math.floor(Math.random() * (max + 1))
+const getRandomItem = (arr) => arr[getRandomInt(arr.length - 1)]
+
+
+const isSamePerson = (a, b) => a.email === b.email
+
+const getRandomOtherPerson = (person, people) => {
+  const possibleMatches = people.filter(p => !isSamePerson(person, p))
+
+  if (possibleMatches.length === 0) {
+    throw new Error('No more matches left')
+  }
+  else {
+    return getRandomItem(possibleMatches)
+  }
+}
+
+const generateMatchList = (people) => {
+  let remainingReceivers = [...people]
+  const matches = []
+
+  people.forEach((giver) => {
+    const receiver = getRandomOtherPerson(giver, remainingReceivers)
+    remainingReceivers = remainingReceivers.filter(p => !isSamePerson(receiver, p))
+    matches.push({giver, receiver})
+  })
+
+  return matches;
+}
+
 export const matchPeople = people => {
-  // Return an array of randomly matched up people.
-  // The array should have objects with the following structure:
-  // { giver: {name: 'NAME_HERE, email: 'EMAIL_HERE'}, receiver: {name: 'NAME_HERE, email: 'EMAIL_HERE'}}
-
-  // Rules:
-  // 1. For every person there has to be a giver
-  // 2. A person cannot be their own giver
-
-  let secretSantaList = []
-  return secretSantaList
+  let secretSantaList = null
+  while(!secretSantaList) {
+    try {
+      secretSantaList = generateMatchList(people)
+    } catch {
+      // an invalid list was generated, try again
+      secretSantaList = null;
+    }
+  }
+  return secretSantaList;
 }
